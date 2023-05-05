ea3dbe4d062af923c3f977abb404b4ee36c22061
wi20269 <sami.haddouti@googlemail.com>
2023-05-05 10:33:21 +0200

    add data name to saved results

--- PatchTST_supervised/run_longExp.py
+++ PatchTST_supervised/run_longExp.py
@@ -120,7 +120,7 @@ if __name__ == "__main__":
             setting = '{}_{}_{}_ft{}_sl{}_ll{}_pl{}_dm{}_nh{}_el{}_dl{}_df{}_fc{}_eb{}_dt{}_{}_{}'.format(
                 args.model_id,
                 args.model,
-                args.data,
+                os.path.splitext(args.data_path)[0],
                 args.features,
                 args.seq_len,
                 args.label_len,
